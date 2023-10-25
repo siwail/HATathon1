a5072d5bbe65de06a8fb1fa73aacd947546f3421
siwail <58031651+siwail@users.noreply.github.com>
2023-10-25 12:43:07 +0300

    First Commit

---
+++ Guild/android/src/com/guild/altruists/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.guild.altruists;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.guild.altruists.Guild;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Guild(), config);
+	}
+}
---
+++ Guild/desktop/src/com/guild/altruists/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.guild.altruists;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.guild.altruists.Guild;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Гильдия Альтруистов");
+		new Lwjgl3Application(new Guild(), config);
+	}
+}
